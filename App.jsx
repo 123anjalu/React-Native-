4a3342399273dc3e2c8ae1321ccc4a140978430a
anjalibora300 <boraanjali275@gmail.com>
2024-11-06 00:29:02 +0530

    View, Button, Text

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize : 30}}>React Native Mobile APP Developer </Text>
+      <Text style={{fontSize : 30}}>Anjali</Text>
+      <Button title="Press Here" />
+    </View>
+  );
+};
+
+export default App;
+
